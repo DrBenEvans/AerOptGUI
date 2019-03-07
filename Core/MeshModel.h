619b9a3931063d3278587c7d07c846c17ec4c4f9
Alex Williams <alexjameswilliams@hotmail.com>
2019-03-07 14:55:55 +0000

    MeshModel.h Documentation Added
    Complete Class, method, and variable descriptions added.

--- Core/MeshModel.h
+++ Core/MeshModel.h
@@ -5,20 +5,54 @@
 #include "Mesh.h"
 #include "BoundaryPointModel.h"
 
+/**
+ * @brief The MeshModel class allows you to interact with a mesh and a set of boundary points where the mesh meets the profile.
+ */
 class MeshModel : public QObject
 {
     Q_OBJECT
 public:
+    /**
+     * @brief MeshModel Constructor Method.
+     * Initialises with an empty boundary point model and no mesh.
+     * @param parent
+     */
     MeshModel(QObject* parent);
+
+    /**
+     * @brief currentMesh Returns the current mesh.
+     * @return the current mesh.
+     */
     Mesh* currentMesh();
+
+    /**
+     * @brief setCurrentMesh Replaces the current mesh with a new one. The previous mesh is discarded.
+     * Emits meshChanged().
+     * @param mesh The new mesh.
+     */
     void setCurrentMesh(Mesh* mesh);
+
+    /**
+     * @brief boundaryPointModel Returns the Boundary Points Model for intertacting
+     * with the points where the mesh meets the profile.
+     * @return Boundary Point Model
+     */
     BoundaryPointModel* boundaryPointModel();
 
 signals:
+    /**
+     * @brief meshChanged Emits if a new mesh is present.
+     */
     void meshChanged();
 
 protected:
+    /**
+     * @brief mMesh The mesh.
+     */
     Mesh* mMesh;
+    /**
+     * @brief mBoundaryPointModel The model for interacting with Boundary Points between the mesh nad profile.
+     */
     BoundaryPointModel* mBoundaryPointModel;
 };
 
